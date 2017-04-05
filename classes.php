110dca93a68c4896e149a77b6cb0d557b190facb
AlexSokolskih <xaam1@ya.ru>
2017-04-05 06:04:55 +0300

    первый

---
+++ classes.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 04.04.2017
+ * Time: 18:48
+ */
+class classes
+{
+
+}
+
+abstract class Car{
+  var  $engine = 0;
+  var  $transmission =0;
+
+    public function move()
+    {
+
+    }
+
+}
+
+abstract class Transmission{
+    
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 04.04.2017
+ * Time: 18:22
+ */
+
